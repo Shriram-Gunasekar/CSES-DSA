912acce1f6b7ec1b0af442b0cba89fb6d7f55a9f
shriramg <gshriram14@gmail.com>
2022-12-25 09:18:54 +0530

    Repetitions partially correct

--- programs/Introductory Problems/Repetitions.py
+++ programs/Introductory Problems/Repetitions.py
@@ -1,1 +1,14 @@
-
+s = input()
+temp = s[0]
+max = 0
+count = 0
+for i in s:
+    if i == temp:
+        count += 1
+    else:
+        temp = i
+        if max < count:
+            max = count
+        count = 0
+print(max)
+        
